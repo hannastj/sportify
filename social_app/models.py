f06268e2a14ba31658314e138d34ad28bb8bef9f
Izzak Fish <2461029f@student.gla.ac.uk>
2025-03-05 12:14:45 +0000

    Integrated Models and User authentication

--- social_app/models.py
+++ social_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 #-------------------BUDDY REQUEST---------------------------
 class BuddyRequest(models.Model):
-    #Users can be a...
-    sender = models.ForeignKey(User, related_name='sent_requests', on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, related_name='received_requests', on_delete=models.CASCADE)
+    # Users can be a...
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_requests', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_requests', on_delete=models.CASCADE)
 
-    #method to show user status
+    # Status of buddy request
     status = models.CharField(max_length=20, choices=(
         ('pending', 'Pending'),
         ('accepted', 'Accepted'),
@@ -15,9 +15,18 @@ class BuddyRequest(models.Model):
     ), default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    #method so user can’t send multiple requests to the same person
     class Meta:
-        unique_together = ('sender', 'receiver')
+        unique_together = ('sender', 'receiver') # User can’t send multiple requests to the same person
 
     def __str__(self):
         return f"{self.sender} -> {self.receiver} ({self.status})"
+    
+        # HANNA new method: When a request is accepted, users are added as buddies
+    def accept(self):
+        # Mark the request as accepted and update both users' buddy lists
+        self.status = 'accepted'
+        self.save()
+
+        # Add each other as buddies
+        self.sender.buddies.add(self.receiver)
+        self.receiver.buddies.add(self.sender)
--- users_app/models.py
+++ users_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser #IZZAK: This is provided by django and provides lots basic attributres we use for users
                                                     #IZZAK: AbstractUser inherits the User class and is used to add Additional Fields required for your User in Database itself
+
         
 #-------------------User Model-----------------    
 #IZZAK: Gym choices for users to select
@@ -27,14 +28,15 @@ class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
     clubs = models.ManyToManyField(SportsClub, blank=True)
+
+     # HANNA added attribute: buddies (friendship links)
+    buddies = models.ManyToManyField(
+        "self",  # Users can be buddies with other users
+        symmetrical=True,  # Friendship is mutual
+        blank=True
+    )
+
     
     def __str__(self):
         return self.username
     
-
-#model in the back end must be made
-
-
-
-
-     #["American Football", "Athletics", "Badminton", "Basketball", "Boat Club", "Boxing", "Canoeing", "Caving", "Cheerleading", "Cricket", "Curling", "Cycling", "Fencing", "Football", "Gaelic Football", "Golf", "Gymnastics", "Handball", "Hares & Hounds", "Hockey", "Judo", "Karate", "Kendo", "Lacrosse", "Mountaineering", "Muay Thai", "Netball", "Riding", "Rugby", "Running", "Sailing", "Shinty", "Shorinji Kempo", "Skiing", "Skydiving", "Snowboarding", "Squash", "Surfing", "Swimming", "Table Tennis", "Taekwondo", "Tennis", "Trampoline", "Triathlon", "Ultimate Frisbee", "Volleyball", "Wakeboarding", "Water Polo", Weightlifting", "Yoga"]
