7a665e31a2ad3d218798889f92387a7f666c3792
Kerr <kerr@Kerrs-MacBook-Pro.local>
2025-03-06 19:19:15 +0000

    New dev_kerr branch. Nuked the old one

--- sportify_project/urls.py
+++ sportify_project/urls.py
@@ -2,22 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from events_app.views import events_view
 from social_app.views import buddyup_view
-from users_app.views import login_view, home_view
-
+from users_app.views import login_view, home_view, profile_view, verification_prompt_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-
-
-    path('login/', login_view, name='login'),
-    path('home/', home_view, name='home'),
-    path('events/', events_view, name='events'),
-    path('buddyup/', buddyup_view, name='buddyup'),
-
-
-
     path('events/', include('events_app.urls')),
     path('social/', include('social_app.urls')),
     path('users/', include('users_app.urls')),
+
+    # MAIN PAGES
+    path('login/', login_view, name='login'),
+    path('home/', home_view, name='home'),
+    path('buddyup/', buddyup_view, name='buddyup'),
+    path('profile/', profile_view, name='profile'),
+    path('events/', events_view, name= 'events'),
+
+    # REDIRECT PAGES
+    path('verification/', verification_prompt_view, name='verification'),
+    path('logout/', logout_view, name='logout'),
 ]
