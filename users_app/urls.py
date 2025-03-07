b6bacd74415e16d55d39bab90dfda39aea277c18
Izzak Fish <2461029f@student.gla.ac.uk>
2025-03-07 10:59:49 +0000

    Working Now

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import login_view, home_view, logout_view
-from django_email_verification import verify
+from django_email_verification import verify_email
 
 app_name = 'users_app'
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('home/', home_view, name='home'), #IZZAK: For consistency rename about to Home
+    path('home/', home_view, name='index'), #IZZAK: For consistency rename about to Home
     path('logout/', logout_view, name='logout'),
-    path("verify/<str:token>/", verify, name="email_verification"), #IZZAK: for email veriication
+    path("verify/<str:token>/", verify_email, name="email_verification"), #IZZAK: for email veriication
 ]
