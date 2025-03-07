aa4f116a0a8540e8df82710b4bd7664705a95177
“Hanna <2572075S@student.gla.ac.uk>
2025-03-07 20:45:18 +0000

    Resolved merge conflict in users_app/urls.py

--- users_app/urls.py
+++ users_app/urls.py
@@ -6,7 +6,11 @@ app_name = 'users_app'
 
 urlpatterns = [
     path('login/', login_view, name='login'),
+<<<<<<< Updated upstream
     path('home/', home_view, name='home'), #IZZAK: For consistency rename about to Home
+=======
+    path('home/', home_view, name='home'),
+>>>>>>> Stashed changes
     path('logout/', logout_view, name='logout'),
     path("verify/<str:token>/", verify_email, name="email_verification"), #IZZAK: for email veriication
 ]
