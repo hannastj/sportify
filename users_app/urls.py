cc2291df9ed31cbca13192f9798aa79133ebb68b
“Hanna <2572075S@student.gla.ac.uk>
2025-03-11 10:19:45 +0000

    Small changes to Home Page

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, home_view, logout_view
+from .views import login_view, home_view, logout_view, profile_view
 
 app_name = 'users_app'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
     path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
 ]
