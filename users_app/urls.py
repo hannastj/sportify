c3feefe6d9e954aa8df245598cdaa343a644eeb8
Kerr <kerr@glaroam2-176-92.wireless.gla.ac.uk>
2025-03-11 14:02:16 +0000

    after merge

--- users_app/urls.py
+++ users_app/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
     path('logout/', logout_view, name='logout'),
-
-
     path('edit/', edit_profile_view, name='edit_profile'),
-    
-
     path('profile/', profile_view, name='profile'),
-
 ]
